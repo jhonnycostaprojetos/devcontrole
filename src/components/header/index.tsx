708b50b72f947f5fdd1072dc0178d3aaed1de76d
Jhonny Costa <jhonnycosta@ibm.com>
2024-01-03 15:37:07 -0300

    Config Auth, Prisma e MongoDb

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,21 @@
+"use client"
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import {FiUser, FiLogOut} from "react-icons/fi"
+import {FiUser, FiLogOut, FiLoader, FiLock} from "react-icons/fi"
+
+
 export function Header(){
+  
+const {status,data} = useSession();
+
+  async function handleLogin(){
+    await signIn()
+  }
+  async function handleLogout(){
+    await signOut();
+  }
+
   return(
     <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm">
       <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
@@ -9,14 +24,28 @@ export function Header(){
           <span className="text-blue-500">DEV</span> CONTROLE
         </h1>
         </Link>
-        <div className="flex items-baseline gap-4">
-        <Link href={"/dashboard"}>
-          <FiUser size={26} color="#4b5563"/>          
-        </Link>
-        <button>
-          <FiLogOut size={26} color= "#4b5563"/>
-        </button>
-        </div>
+
+        {status === "loading" &&(
+          <button className="animate-spin">
+            <FiLoader size ={26} color="#4b5563" />
+          </button>
+        )}
+          {status === "unauthenticated" &&(
+          <button onClick={handleLogin}>
+            <FiLock size ={26} color="#4b5563" />
+          </button>
+        )}
+
+          {status === "authenticated" && (
+               <div className="flex items-baseline gap-4">
+               <Link href={"/dashboard"}>
+                 <FiUser size={26} color="#4b5563"/>          
+               </Link>
+               <button onClick={handleLogout}>
+                 <FiLogOut size={26} color= "#ff2313"/>
+               </button>
+               </div>
+          )}
    
       </div>
     </header>
